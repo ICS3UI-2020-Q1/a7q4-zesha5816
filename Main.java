e143779548f1b3d4d6ad39d5210f3732877c5a6e
zesha5816 <zesha5816@wrdsb.ca>
2020-10-13 15:44:09 +0000

    Finished the example

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
 /**
- *
- * @author 
+ * a method named lastDigit that returns the last digit of an integer.
+ * @author Ahmad Zeshan
  */
 public class Main {
 
+    
+   public static int lastDigit(int userInt){
+        //find the last digit of the number
+				 int ans = userInt % 10;
+				// return the last digit
+				return ans;
+	 }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+   //store last num as the last digit of the given value 
+	 int lastNum = lastDigit(38);
+	 //print out the value to the user 
+	 System.out.println(lastNum);
+	}
 }
